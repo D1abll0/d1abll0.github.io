bc67f9a199d8b411227b13a602e43aa9984aa455
D1abll0 <94897456+D1abll0@users.noreply.github.com>
2026-08-20 11:22:38 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,12 +1,7 @@
-import { DiscordSDK }
-from 'https://esm.sh/@discord/embedded-app-sdk';
+import { DiscordSDK } from './discord-sdk.js';
 
 const CLIENT_ID = '1539377304646586428';
-
 const discordSdk = new DiscordSDK(CLIENT_ID);
-
 await discordSdk.ready();
-
 console.log('Discord SDK ready');
-
 export { discordSdk };
